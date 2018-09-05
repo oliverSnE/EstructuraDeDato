d0584741ca2d4dc2c83ce208cecb4da0f806ac5b
OliverSnE Burgara <sinue_oliver@hotmail.com>
2018-09-04 19:59:08 -0700

    actualizacion

--- EstructuraDeDatos/EstructuraDeDatos/Form1.Designer.cs
+++ EstructuraDeDatos/EstructuraDeDatos/Form1.Designer.cs
@@ -28,21 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.msMenu = new System.Windows.Forms.MenuStrip();
+            this.unidad1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.unidad2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.unidad3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.unidad4ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.unidad5ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtBienvenida = new System.Windows.Forms.TextBox();
+            this.msMenu.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // msMenu
+            // 
+            this.msMenu.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.msMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.unidad1ToolStripMenuItem,
+            this.unidad2ToolStripMenuItem,
+            this.unidad3ToolStripMenuItem,
+            this.unidad4ToolStripMenuItem,
+            this.unidad5ToolStripMenuItem});
+            this.msMenu.Location = new System.Drawing.Point(0, 0);
+            this.msMenu.Name = "msMenu";
+            this.msMenu.Size = new System.Drawing.Size(800, 28);
+            this.msMenu.TabIndex = 1;
+            this.msMenu.Text = "menuStrip1";
+            // 
+            // unidad1ToolStripMenuItem
+            // 
+            this.unidad1ToolStripMenuItem.Name = "unidad1ToolStripMenuItem";
+            this.unidad1ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.unidad1ToolStripMenuItem.Text = "Unidad 1";
+            // 
+            // unidad2ToolStripMenuItem
+            // 
+            this.unidad2ToolStripMenuItem.Name = "unidad2ToolStripMenuItem";
+            this.unidad2ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.unidad2ToolStripMenuItem.Text = "Unidad 2";
+            // 
+            // unidad3ToolStripMenuItem
+            // 
+            this.unidad3ToolStripMenuItem.Name = "unidad3ToolStripMenuItem";
+            this.unidad3ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.unidad3ToolStripMenuItem.Text = "Unidad 3";
+            // 
+            // unidad4ToolStripMenuItem
+            // 
+            this.unidad4ToolStripMenuItem.Name = "unidad4ToolStripMenuItem";
+            this.unidad4ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.unidad4ToolStripMenuItem.Text = "Unidad 4";
+            // 
+            // unidad5ToolStripMenuItem
+            // 
+            this.unidad5ToolStripMenuItem.Name = "unidad5ToolStripMenuItem";
+            this.unidad5ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.unidad5ToolStripMenuItem.Text = "Unidad 5";
+            // 
+            // txtBienvenida
+            // 
+            this.txtBienvenida.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtBienvenida.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtBienvenida.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBienvenida.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txtBienvenida.Location = new System.Drawing.Point(13, 32);
+            this.txtBienvenida.Multiline = true;
+            this.txtBienvenida.Name = "txtBienvenida";
+            this.txtBienvenida.Size = new System.Drawing.Size(775, 406);
+            this.txtBienvenida.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtBienvenida);
+            this.Controls.Add(this.msMenu);
+            this.MainMenuStrip = this.msMenu;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.msMenu.ResumeLayout(false);
+            this.msMenu.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip msMenu;
+        private System.Windows.Forms.ToolStripMenuItem unidad1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem unidad2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem unidad3ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem unidad4ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem unidad5ToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtBienvenida;
     }
 }
 
--- EstructuraDeDatos/EstructuraDeDatos/Form1.cs
+++ EstructuraDeDatos/EstructuraDeDatos/Form1.cs
@@ -16,9 +16,22 @@ namespace EstructuraDeDatos
         {
             InitializeComponent();
         }
+        private void responsive()
+        {
+            txtBienvenida.Location = new Point(1,1);
+            txtBienvenida.Width = this.Width - msMenu.Height;
+            txtBienvenida.Height = this.Height - 1;
+            txtBienvenida.Enabled = false;
+            txtBienvenida.Text = "El alumno desarrollará aplicaciones que organicen información con base a estructuras de datos abstractas empleando el paradigma orientado a objetos para agilizar el acceso a los datos almacenados.";
+
+
+
+        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            responsive();
+           
 
         }
     }
